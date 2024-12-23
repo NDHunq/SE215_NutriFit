e000ec222d8061d1502bcd6d776feb2b4f346d9c
NDHunq <22520514@gm.uit.edu.vn>
2024-12-23 21:54:20 +0700

    project structure

---
+++ lib/core/configs/constants/app_infor1.dart
@@ -0,0 +1,7 @@
+// class AppInfor1 {
+//   static const double horizontal_padding = 15;
+//   static const tasker_token =
+//       "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsInBob25lTnVtYmVyIjoiMDM0NTY2NDAyNSIsInJvbGUiOiJSMiIsImV4cGlyZXNJbiI6IjMwZCIsImlhdCI6MTcyODI5MDg0MH0.nr9h-DAJ9OuypPzMdg-4LjLREcdzEuB1-W0xisgtW1g";
+//   static const user_token =
+//       'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjUsInBob25lTnVtYmVyIjoiMDM0NTY2NDAyNSIsInJvbGUiOiJSMSIsImV4cGlyZXNJbiI6IjMwZCIsImlhdCI6MTcyODIyMzI3N30.HPD25AZolhKCteXhFbF34zMyh2oewByvVHKBrFfET88';
+// }
---
+++ lib/core/configs/text/app_text_style.dart
@@ -0,0 +1,46 @@
+// import 'package:flutter/material.dart';
+
+// class AppTextStyle {
+//   static const tieudebox = TextStyle(
+//       fontFamily: 'Inter',
+//       fontSize: 18,
+//       fontWeight: FontWeight.bold,
+//       color: Colors.black,
+//       decoration: TextDecoration.none);
+//   static const tieudebox20 = TextStyle(
+//       fontFamily: 'Inter',
+//       fontSize: 20,
+//       fontWeight: FontWeight.bold,
+//       color: Colors.black,
+//       decoration: TextDecoration.none);
+//   static const tieudebox25 = TextStyle(
+//       fontFamily: 'Inter',
+//       fontSize: 21,
+//       fontWeight: FontWeight.bold,
+//       color: Colors.black,
+//       decoration: TextDecoration.none);
+//   static const textthuong = TextStyle(
+//       fontFamily: 'Inter',
+//       fontSize: 16,
+//       color: Colors.black,
+//       decoration: TextDecoration.none,
+//       fontWeight: FontWeight.normal);
+//   static const textthuongxam = TextStyle(
+//       fontFamily: 'Inter',
+//       fontSize: 16,
+//       color: AppColors.xam72,
+//       decoration: TextDecoration.none,
+//       fontWeight: FontWeight.normal);
+//   static const textnhoxam = TextStyle(
+//       fontFamily: 'Inter',
+//       fontSize: 15,
+//       color: AppColors.xam72,
+//       decoration: TextDecoration.none,
+//       fontWeight: FontWeight.normal);
+//   static const textthuongxanhmain = TextStyle(
+//       fontFamily: 'Inter',
+//       fontSize: 16,
+//       color: AppColors.xanh_main,
+//       decoration: TextDecoration.none,
+//       fontWeight: FontWeight.normal);
+// }
